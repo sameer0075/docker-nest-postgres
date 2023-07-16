8de0dc01102f64a6a89e81e2bd4e5b422c76b17e
sameer0075 <ismail.aijaz@simplextechnologysolutions@gmail.com>
2023-07-16 22:12:59 +0500

    prevent multiple requests at same time

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import * as dotenv from 'dotenv';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { HttpExceptionFilter } from './common/helper/exception-filter';
 import { ExternalModules } from './common/Modules/external.modules';
 import { InternalModules } from './common/Modules/internal.modules';
 import * as compression from 'compression';
+import { PreventMultipleClicksInterceptor } from './common/helper/decorators/prevent-multiple-api-calls.decorator';
 
 dotenv.config();
 @Module({
@@ -14,6 +15,10 @@ dotenv.config();
     {
       provide: APP_FILTER,
       useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: PreventMultipleClicksInterceptor,
     },
   ],
 })
